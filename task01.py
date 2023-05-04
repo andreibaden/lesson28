32f4dedcacdbe05838c6115beabdb78e6dd10ee7
andreibaden@gmail.com <itstep_student@outlook.com>
2023-05-04 21:14:25 +0300

    Initial commit

---
+++ task01.py
@@ -0,0 +1,15 @@
+from human import Human
+from manager import Manager
+from human_creator import HumanCreator
+
+ls = HumanCreator.create(20)
+
+for human in ls:
+    print(human)
+
+adult = Manager.count_adult(ls)
+underage = Manager.count_underage(ls)
+
+print(f"Adult = {adult}")
+print(f"Underage = {underage}")
+
